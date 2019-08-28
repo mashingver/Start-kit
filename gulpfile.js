833a9f1e06337a8b168a9bc2f18649164a49ae8c
Oleksandr Chernysh <oc@unicsoft.net>
2019-08-28 22:55:01 +0300

    Updated to Gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,199 +1,245 @@
-var gulp = require('gulp'),
-	config = require('./gulp.config')(),
-	del = require('del'),
-	browserSync = require("browser-sync"),
-	reload = browserSync.reload,
-	extender = require('gulp-html-extend'),
-	mainBowerFiles = require("main-bower-files"),
-	spritesmith = require('gulp.spritesmith'),
-	stylish = require('jshint-stylish'),
-	$ = require('gulp-load-plugins')({lazy: true});
+const gulp = require("gulp");
+const del = require("del");
+const browserSync = require("browser-sync").create();
+const extender = require("gulp-html-extend");
+const $ = require("gulp-load-plugins")({ lazy: true });
+const pngquant = require("imagemin-pngquant");
+const svgSprite = require("gulp-svg-sprite");
 
-// Lint SCSS
-gulp.task('scsslint', function () {
-	log('Linting SCSS');
-	// TODO: Choose scss linter
-});
+const buildFolder = "./build/";
+const srcFolder = "./src/";
 
-// Lint JS
-gulp.task('jshint', function () {
-	log('Linting JS');
-	return gulp.src(config.src.js)
-		.pipe($.jshint())
-		.pipe($.jshint.reporter(stylish))
-	//todo: write some rules for linting javascript
-});
+const config = {
+  env: "development",
+  build: {
+    html: buildFolder,
+    css: buildFolder + "css/",
+    js: buildFolder + "js/",
+    images: buildFolder + "images/",
+    favicons: buildFolder,
+    fonts: buildFolder + "fonts/"
+  },
+  src: {
+    html: srcFolder + "html/index.html",
+    scss: srcFolder + "scss/styles.scss",
+    js: srcFolder + "js/app.js",
+    images: srcFolder + "images/**/*.*",
+    sprite: srcFolder + "images/",
+    favicons: srcFolder + "favicons/*.*",
+    icons: srcFolder + "icons/**/*.svg",
+    fonts: srcFolder + "fonts/**/*.{eot,svg,ttf,otf,woff}"
+  },
+  watch: {
+    html: srcFolder + "html/**/*.html",
+    scss: srcFolder + "scss/**/*.scss",
+    js: srcFolder + "js/**/*.js",
+    images: srcFolder + "images/**/*.*",
+    sprite: srcFolder + "icons/**/*.*"
+  },
+  clean: buildFolder + "*",
+  autoprefixer: {
+    overrideBrowserslist: [
+      "last 1 version",
+      "> 1%",
+      "maintained node versions",
+      "not dead"
+    ]
+  },
+  browserSyncConfig: {
+    server: {
+      baseDir: buildFolder
+    },
+    host: "localhost",
+    port: 9000,
+    open: true,
+    debug: true
+  },
+  cssimport: {
+    matchPattern: "*.css"
+  },
+  extender: {
+    annotations: true,
+    verbose: false
+  },
+  imagemin: {
+    progressive: true,
+    svgoPlugins: [
+      {
+        removeViewBox: false
+      }
+    ],
+    use: [pngquant()],
+    interlaced: true
+  },
+  svgSprite: {
+    mode: {
+      css: {
+        bust: false,
+        render: {
+          scss: {
+            dest: "../scss/core/_sprite.scss"
+          }
+        },
+        dest: ".",
+        sprite: "../images/sprite.svg",
+      },
+
+    }
+  }
+};
+
+// BrowserSyncServer
+function browserSyncServer(done) {
+  log('Starting a server');
+  browserSync.init(config.browserSyncConfig);
+  done();
+}
 
 // Compile HTML
-gulp.task('html', function () {
-	log('Compiling HTML');
-	return gulp.src(config.src.html)
-		.pipe($.plumber())
-		.pipe(extender(config.extender))
-		.pipe($.if(config.env === 'production', $.htmlmin()))
-		.on('error', $.util.log)
-		.pipe(gulp.dest(config.build.html))
-		.pipe(reload({stream: true}));
-});
+function html() {
+  log("Compiling HTML");
+  return gulp
+    .src(config.src.html)
+    .pipe($.plumber())
+    .pipe(extender(config.extender))
+    .pipe($.if(config.env === "production", $.htmlmin()))
+    .on("error", $.util.log)
+    .pipe(gulp.dest(config.build.html))
+    .pipe(browserSync.stream());
+}
 
 // Compile SCSS
-gulp.task('scss', ['scsslint'], function () {
-	log('Compiling SCSS --> CSS');
-	return gulp.src(config.src.scss)
-		.pipe($.plumber())
-		.pipe($.sourcemaps.init())
-		.pipe($.sass())
-		.pipe($.cssimport(config.cssimport))
-		.pipe($.autoprefixer(config.autoprefixer))
-		.pipe($.cssnano())
-		.pipe($.rename({extname: '.min.css'}))
-		.pipe($.sourcemaps.write('./'))
-		.pipe(gulp.dest(config.build.css))
-		.pipe(reload({stream: true}));
-});
+function scss() {
+  log("Compiling SCSS --> CSS");
+  return gulp
+    .src(config.src.scss)
+    .pipe($.plumber())
+    .pipe($.sourcemaps.init())
+    .pipe($.sass())
+    .pipe($.cssimport(config.cssimport))
+    .pipe($.autoprefixer(config.autoprefixer))
+    .pipe($.cssnano())
+    .pipe($.rename({ extname: ".min.css" }))
+    .pipe($.sourcemaps.write("./"))
+    .on("error", $.util.log)
+    .pipe(gulp.dest(config.build.css))
+    .pipe(browserSync.stream());
+}
 
 // Compile JS
-gulp.task('js', ['jshint'], function () {
-	log('Compiling JS');
-	return gulp.src(config.src.js)
-		.pipe($.plumber())
-		.pipe($.rigger())
-		.pipe($.if(config.env === 'production', $.uglify()))
-		.on('error', $.util.log)
-		.pipe(gulp.dest(config.build.js))
-		.pipe(reload({stream: true}));
-});
+function js() {
+  log("Compiling JS");
+  return gulp
+    .src(config.src.js)
+    .pipe($.plumber())
+    .pipe($.rigger())
+    .pipe($.uglify())
+    .pipe($.rename({ extname: ".min.js" }))
+    .on("error", $.util.log)
+    .pipe(gulp.dest(config.build.js))
+    .pipe(browserSync.stream());
+}
 
 // Optimize Images
-gulp.task('images', function () {
-	log('Optimizing Images');
-	return gulp.src(config.src.img)
-		.pipe($.plumber())
-		.pipe($.if(config.env === 'production', $.imagemin(config.imagemin)))
-		.pipe(gulp.dest(config.build.img))
-		.pipe(reload({stream: true}));
-	//todo: organize images, icons and sprites
-});
+function images() {
+  log("Optimizing Images");
+  return gulp
+    .src(config.src.images)
+    .pipe($.plumber())
+    .pipe($.imagemin(config.imagemin))
+    .pipe(gulp.dest(config.build.images))
+    .pipe(browserSync.stream());
+}
 
-// Generate sprite
-gulp.task('sprite', function () {
-	log('Creating icons sprite');
-	gulp.src(config.src.sprite_src)
-		.pipe($.plumber())
-		.pipe(spritesmith(config.spritesmith))
-		.pipe(gulp.dest(config.src.sprite));
-	
-	gulp.src('./src/sprite/sprite.png')
-		.pipe(gulp.dest('./src/images/'));
+// Copy favicons
+function favicons() {
+  log("Copying favicons");
+  return gulp
+      .src(config.src.favicons)
+      .pipe(gulp.dest(config.build.favicons));
+}
 
-	gulp.src('./src/sprite/_sprite.scss')
-		.pipe(gulp.dest('./src/scss/'));
-	
-	
-	//todo: create better sprite task
-	
-});
+function sprite() {
+  return gulp
+    .src(config.src.icons)
+    .pipe(svgSprite(config.svgSprite))
+    .pipe(gulp.dest(config.src.sprite));
+}
 
 // Copy Fonts
-gulp.task('fonts', function () {
-	log('Copying fonts');
-	gulp.src(config.src.fonts)
-		.pipe(gulp.dest(config.build.fonts));	
-	
-	// todo: think of better fonts task
-});
-
-// Compile vendors CSS
-gulp.task("vendors:css", function () {
-	log('Compiling vendors css files');
-	return gulp.src(mainBowerFiles({
-			filter: /.*\.css$/
-		}))
-		.pipe($.plumber())
-		.pipe($.concat('vendors.min.css'))
-		.pipe($.cssnano())
-		.pipe(gulp.dest(config.build.css));
-});
-
-// Compile vendors JS
-gulp.task("vendors:js", function () {
-	log('Compiling vendors javascript files');
-	return gulp.src(mainBowerFiles({
-			filter: /.*\.js$/
-		}))
-		.pipe($.plumber())
-		.pipe($.concat('vendors.min.js'))
-		.pipe($.uglify())
-		.pipe(gulp.dest(config.build.js));
-});
-
-// Compile vendors
-gulp.task('vendors', function () {
-	log('Compiling vendors files');
-	gulp.start('vendors:css');
-	gulp.start('vendors:js');
-});
+function fonts() {
+  log("Copying fonts");
+  return gulp
+      .src(config.src.fonts)
+      .pipe(gulp.dest(config.build.fonts));
+}
 
 // Compile build
-gulp.task('build', function () {
-	log('Compiling build');
-	gulp.start('html');
-	gulp.start('sprite');
-	gulp.start('images');
-	gulp.start('fonts');
-	gulp.start('scss');
-	gulp.start('js');
-});
+function build(done) {
+  log("Compiling build");
+  gulp.series(html, scss, js, sprite, images, favicons, fonts)(done);
+}
 
-// Server
-gulp.task('server', function () {
-	log('Starting a server');
-	browserSync(config.browserSyncConfig);
-});
+// Compile vendors CSS
+function vendorsCss() {
+  log("Compiling vendors css files");
+  return gulp
+    .src()
+    .pipe($.plumber())
+    .pipe($.concat("vendors.min.css"))
+    .pipe($.cssnano())
+    .pipe(gulp.dest(config.build.css));
+}
 
-// Watch files changes
-gulp.task('watch', function () {
-	log('Watching html, scss, js and image files');
-	$.watch([config.watch.html], function (event, cb) {
-		gulp.start('html');
-	});
-	$.watch([config.watch.scss], function (event, cb) {
-		gulp.start('scss');
-	});
-	$.watch([config.watch.js], function (event, cb) {
-		gulp.start('js');
-	});
-	$.watch([config.watch.img], function (event, cb) {
-		gulp.start('images');
-	});
-	$.watch([config.watch.sprite], function (event, cb) {
-		gulp.start('sprite');
-	});	
-	$.watch([config.watch.fonts], function (event, cb) {
-		gulp.start('fonts');
-	});	
-	
-});
+// Compile vendors JS
+function vendorsJs() {
+  log("Compiling vendors javascript files");
+  return gulp
+    .src()
+    .pipe($.plumber())
+    .pipe($.concat("vendors.min.js"))
+    .pipe($.uglify())
+    .pipe(gulp.dest(config.build.js));
+}
+
+function vendors(done) {
+  log("Compiling vendors");
+  gulp.parallel(vendorsCss, vendorsJs)(done);
+}
 
 // Clean build folder
-gulp.task('clean', function () {
-	var files = config.clean;
-	log('Cleaning: ' + $.util.colors.blue(files));
-	del(files);
-});
+function clean(done) {
+  const files = config.clean;
+  log("Cleaning: " + $.util.colors.blue(files));
+  del(files);
+  done();
+}
 
-// Gulp default
-gulp.task('default', ['build', 'server', 'watch']);
+// Watch files changes
+function watchFiles() {
+  log("Watching html, scss, js, images and sprite files");
+  gulp.watch(config.watch.html, html);
+  gulp.watch(config.watch.scss, scss);
+  gulp.watch(config.watch.js, js);
+  gulp.watch(config.watch.images, images);
+  gulp.watch(config.watch.sprite, sprite);
+}
+
+// Gulp tasks
+exports.default = gulp.series(clean, build, gulp.parallel(watchFiles, browserSyncServer));
+exports.production = gulp.series(clean, build, vendors);
+exports.vendors = vendors;
+
 
 //Helpers
 function log(msg) {
-	if (typeof(msg) === 'object') {
-		for (var item in msg) {
-			if (msg.hasOwnProperty(item)) {
-				$.util.log($.util.colors.blue(msg[item]));
-			}
-		}
-	} else {
-		$.util.log($.util.colors.blue(msg));
-	}
+  if (typeof msg === "object") {
+    for (let item in msg) {
+      if (msg.hasOwnProperty(item)) {
+        $.util.log($.util.colors.blue(msg[item]));
+      }
+    }
+  } else {
+    $.util.log($.util.colors.blue(msg));
+  }
 }
